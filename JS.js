c56faa27e35b880b32aee013df86f9bdceffa901
Actro25 <skiril502@gmail.com>
2025-02-21 16:35:34 +0200

    none

---
+++ JS.js
@@ -0,0 +1,16 @@
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     let preloadDIVs = document.querySelectorAll('.ldio-yzaezf3dcmj div');
+
+//     preloadDIVs.forEach((div) => {
+//         div.addEventListener('animationiteration', () => {
+//             div.style.borderRadius = `${getRandomInt(60)}% ${getRandomInt(60)}% ${getRandomInt(60)}% ${getRandomInt(60)}% / 
+//                 ${getRandomInt(60)}% ${getRandomInt(60)}% ${getRandomInt(60)}% ${getRandomInt(60)}%`;
+
+//             console.log("borderRadius змінено після повтору анімації!");
+//         });
+//     });
+// });
